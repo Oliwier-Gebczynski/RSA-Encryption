6c56dbf8fba4112ad880d2588a073682102243af
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2023-02-25 23:32:21 +0100

    start project

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//TODO
+//- read file,
+//- encrypt and decrypt function by symmetric algorithm (idk which),
+//- save to file,
+//- next encrypt key from symmetric algorithm by RSA,
+//- decrypt RSA function,
+//- README.md
+
+
+
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by oliwi on 2/25/2023.
+//
+
+#ifndef RSA_ENCRYPTION_MAIN_H
+#define RSA_ENCRYPTION_MAIN_H
+
+#endif //RSA_ENCRYPTION_MAIN_H
