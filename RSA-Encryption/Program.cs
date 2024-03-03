81669e4dc6b730a71c563908b84d8044fa59e147
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-03-03 21:36:57 +0100

    Prepared data structure for implementation in C#

---
+++ RSA-Encryption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RSA_Encryption
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("RSA-Encryption");
+        }
+    }
+}
